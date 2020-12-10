fda903af94b05ec97928c7ce67458bb6c4b2c593
southWind8 <71750018+southWind8@users.noreply.github.com>
2020-12-10 17:22:18 +0800

    Add files via upload
    前端起步

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-container>
+      <router-view />
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  })
+}
+</script>
---
+++ plugins/vuetify.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+// import colors from 'vuetify/lib/util/colors'
+
+Vue.use(Vuetify)
+
+export default new Vuetify({
+  
+})
